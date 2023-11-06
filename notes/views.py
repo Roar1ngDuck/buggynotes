72bcafb647313c7a2a5f4d774a71f2cde451bfed
Niko <niko.summanen1@gmail.com>
2023-11-06 20:41:40 +0200

    Add basic note listing

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Note
 
-# Create your views here.
+def index(request):
+    notes = [
+    Note(id=1, title="Grocery List", content="Milk, Eggs, Bread, Butter, Apples"),
+    Note(id=2, title="Workout Routine", content="Monday: Chest\nTuesday: Back\nWednesday: Rest\nThursday: Arms\nFriday: Legs"),
+    Note(id=3, title="Book Recommendations", content="1. '1984' by George Orwell\n2. 'To Kill a Mockingbird' by Harper Lee\n3. 'The Great Gatsby' by F. Scott Fitzgerald")
+]
+
+    return render(request, "notes/index.html", {'notes': notes})
+
