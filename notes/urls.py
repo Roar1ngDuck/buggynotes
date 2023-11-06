aa27ded9f6a375f61f974e691ade9fd52c63b483
Niko <niko.summanen1@gmail.com>
2023-11-06 20:59:43 +0200

    Implement basic login

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Note
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
+@login_required
 def index(request):
     notes = [
     Note(id=1, title="Grocery List", content="Milk, Eggs, Bread, Butter, Apples"),
@@ -11,3 +18,15 @@ def index(request):
 
     return render(request, "notes/index.html", {'notes': notes})
 
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('index')  # Redirect to login page after signup
+    template_name = 'registration/signup.html'  # Path to the signup template
+
+    def form_valid(self, form):
+        valid = super(SignUpView, self).form_valid(form)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return valid
